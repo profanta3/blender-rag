b3eca26425d75e63029eab40a700342a798f56cd
profanta <me@profanta.de>
2024-10-19 21:59:23 +0200

    feat: rename pages dir

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-retrieval_page = st.Page("pages/retrieval.py", title="Retrieval", icon="🔍")
-prompt_page = st.Page("pages/prompt.py", title="Prompt", icon="📃")
-chat_page = st.Page("pages/chat.py", title="Chat", icon="🤖")
+retrieval_page = st.Page("routes/retrieval.py", title="Retrieval", icon="🔍")
+prompt_page = st.Page("routes/prompt.py", title="Prompt", icon="📃")
+chat_page = st.Page("routes/chat.py", title="Chat", icon="🤖")
 
 pg = st.navigation([chat_page, retrieval_page, prompt_page])
 pg.run()
