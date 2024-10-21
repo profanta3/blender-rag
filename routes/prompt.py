608868f291e36a393a7bb615cf648254d667ee43
profanta <me@profanta.de>
2024-10-22 01:16:58 +0200

    feat(ui): submit settings and prompt with button

--- routes/prompt.py
+++ routes/prompt.py
@@ -7,13 +7,15 @@ st.title("Edit Prompt template")
 
 
 rag_app = st.session_state["rag_app"]
+with st.form("my_form"):
+    txt = st.text_area(
+        label="Prompt Template",
+        value=st.session_state["rag_app"].prompt.template,
+        height=400,
+        label_visibility="collapsed",
+    )
 
-txt = st.text_area(
-    label="Prompt Template",
-    value=st.session_state["rag_app"].prompt.template,
-    height=400,
-)
-
-if txt and txt != st.session_state["rag_app"].prompt.template:
-    st.session_state["rag_app"].prompt.template = txt
-    st.toast("Prompt saved!")
+    submitted = st.form_submit_button("Save")
+    if submitted:
+        st.session_state["rag_app"].prompt.template = txt
+        st.toast("Prompt saved!")
--- routes/settings.py
+++ routes/settings.py
@@ -7,14 +7,15 @@ from rag_app import RagApp
 rag_app: RagApp = st.session_state["rag_app"]
 
 st.title("Settings")
+with st.form("my_form"):
+    base_url = st.text_input("OpenAI URL", rag_app.get_openai_base_url())
 
-if base_url := st.text_input("OpenAI URL", rag_app.get_openai_base_url()):
-    rag_app.set_openai_base_url(base_url)
+    model_name = st.selectbox(
+        "Model", [m.id for m in rag_app.get_available_models().data]
+    )
 
-    st.toast(f"URL successfully set to {base_url}")
-
-if model_name := st.selectbox(
-    "Model", [m.id for m in rag_app.get_available_models().data], key="model_name"
-):
-    rag_app.set_model(model_name)
-    st.toast(f"Model set to {model_name}")
+    submitted = st.form_submit_button("Save")
+    if submitted:
+        rag_app.set_openai_base_url(base_url)
+        rag_app.set_model(model_name)
+        st.toast("Settings saved!")
