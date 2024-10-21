e585fa68e70f47ede18b3b3bdd4cc104b4de8497
profanta <me@profanta.de>
2024-10-21 22:19:14 +0200

    feat: use ollama embedding model

--- models.py
+++ models.py
@@ -3,14 +3,17 @@ from lancedb.pydantic import LanceModel, Vector
 from lancedb.embeddings import get_registry
 from pydantic import BaseModel
 
-model = (
-    get_registry().get("sentence-transformers").create(name="BAAI/bge-small-en-v1.5")
-)
+# model = (
+#     get_registry()
+#     .get("sentence-transformers")
+#     .create(name="BAAI/bge-large-en-v1.5")  # BAAI/bge-small-en-v1.5")
+# )
+model = get_registry().get("ollama").create(name="nomic-embed-text")
 
 
 class Document(LanceModel):
     text: str = model.SourceField()
-    vector: Vector(384) = model.VectorField()
+    vector: Vector(model.ndims()) = model.VectorField()
 
 
 class Prompt(BaseModel):
