387af8376ead8bf4eee68969950964144389b5f8
profanta <karl@neukoelln-berlin.de>
2024-10-22 12:09:29 +0200

    feat(ui-retrieval): plot pca 3d and format query display

--- routes/retrieval.py
+++ routes/retrieval.py
@@ -2,10 +2,10 @@ import os
 import streamlit as st
 
 from models import model
-from rag_app import RagApp
+
 from utils import pca
 import pandas as pd
-
+import plotly.express as px
 
 st.title("Latest retrieval result")
 
@@ -20,14 +20,18 @@ if query:
     st.session_state["latest_rag"] = results[0].text
     st.session_state["retrieval_results"] = results
 
-if "query" in st.session_state:
-    with st.chat_message("user"):
-        st.write(st.session_state.query)
+
+def write_latest_query():
+    if "query" in st.session_state:
+        with st.chat_message("user"):
+            st.write(st.session_state.query)
+
 
 tab1, prompt_tab, tab2 = st.tabs(["Retrieved Document", "Generated Prompt", "PCA plot"])
 
 if "latest_rag" in st.session_state:
     with tab1:
+        write_latest_query()
         st.code(st.session_state.latest_rag, language="markdown")
 else:
     with tab1:
@@ -41,24 +45,26 @@ def plot_pca():
     vectors = [result.vector for result in st.session_state.retrieval_results]
 
     query_emb = model.generate_embeddings([query])
-    pca_data = pca(query_emb + vectors)
+    pca_data = pca(query_emb + vectors, 3)
 
     # Create a DataFrame for the PCA data
-    df = pd.DataFrame(pca_data, columns=["PC1", "PC2"])
+    df = pd.DataFrame(pca_data, columns=["PC1", "PC2", "PC3"])
 
     # Add a column for color
     df["color"] = ["#FF0000"] + ["#FFFFFF"] + ["#0000FF"] * (len(df) - 2)
     df["size"] = [5] + [5] + [1] * (len(df) - 2)
+    df["text"] = ["Query"] + ["Used Doc"] + [""] * (len(df) - 2)
 
-    # Plot the scatter chart
-    st.scatter_chart(
-        df, x="PC1", y="PC2", color="color", size="size", use_container_width=True
+    fig = px.scatter_3d(
+        df, x="PC1", y="PC2", z="PC3", color="color", size="size", text="text"
     )
+    st.plotly_chart(fig, use_container_width=True)
 
 
 # add pca plot for 10 search results
 if "retrieval_results" in st.session_state and query:
     with tab2:
+        write_latest_query()
         plot_pca()
 else:
     with tab2:
@@ -74,6 +80,7 @@ else:
 with prompt_tab:
     prompt = rag_app.get_latest_prompt()
     if prompt:
+        write_latest_query()
         st.code(rag_app.get_latest_prompt(), language="markdown")
     else:
         st.write("No prompt available - please enter a query to generate a prompt.")
